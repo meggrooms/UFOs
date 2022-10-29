6adbd6bd34494f9fb9270c85df0125ab9eaff995
Meg Grooms-Gangloff <102067932+meggrooms@users.noreply.github.com>
2022-10-28 21:01:03 -0700

    Add files via upload

--- Resources/app.js
+++ Resources/app.js
@@ -1,5 +1,6 @@
 // Align code ()
 // import data from data.js
+// const cannpt be reapplied or reassigned
 const tableData = data; //variable is tableData, assigned to data
 
 // Reference HTML table using D3
@@ -10,23 +11,27 @@ function buildTable(data){
 
 }
 
-//clear data to make room for table with data
+// build then clear data to make room for table with data
 function.buildTable(data){
     tbody.html(" ");
 
 }
 
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append("tr");   // adds table row to table
 
-//start loop on data - cleaner to read with =>
-data.forEach((dataRow) => {          //variable => {}
-    let row = tbody.append("tr");       //make a row (tbody is appended to add a "tr")  
-    Object.values(dataRow).forEach((val) => {    // start code (obj), ref one obj of array
-      let cell = row.append("td");     //append value of obj to cell
-      cell.text(val);      //append data into td
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {  // start code (obj), ref one obj of array
+      let cell = row.append("td"); //append value of obj to cell
+      cell.text(val);   // //append TEXT data into td
       }
     );
   });
-
+}
 
 
 
